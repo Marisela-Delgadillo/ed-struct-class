a36cffe8252927ae5b0f507db5fb5ba4c4062fab
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-09-08 17:04:46 -0700

    struct class

--- src/main.cc
+++ src/main.cc
@@ -1,33 +1,40 @@
-#include<iostream>
+#include <iostream>
 //#include "Character.hh"
 #include "Hero.hh"
 
 int main()
+{
+    //Heap necesita la paa=labra new si se hace en la memoria pero sin tener limite como el stack
+    //El stack se unsa cuando creas algo si new sirectamente en funciones o variables globales
+
+    Hero *hero1{new Hero(5, 3, 1, 2, 3, 1, new std::string[4]{{"fire ball"}, {"fenix tail"}, {"war growl"}, {"fly"}})};
+
+    Hero **heroList{
+        new Hero *[4] {
+            new Hero(5, 3, 1, 2, 3, 1, new std::string[4]{{"fire ball"}, {"fenix tail"}, {"war growl"}, {"fly"}}),
+                new Hero(4, 3, 1, 2, 3, 1, new std::string[4]{{"water heal"}, {"erik do something"}, {"rays sword"}, {"fdgdf"}}),
+                new Hero(7, 3, 1, 2, 3, 1, new std::string[4]{{"ksdjfhksj"}, {"lml"}, {"loremipsum"}, {"fly2"}}),
+                new Hero(2, 3, 1, 2, 3, 1, new std::string[4]{{"fire ball"}, {"ddd"}, {"aaaa"}, {"bbbbb"}})
+        }};
+
+    for (int i{}; i < 4; i++)
     {
-        //Heap necesita la paa=labra new si se hace en la memoria pero sin tener limite como el stack
-        //El stack se unsa cuando creas algo si new sirectamente en funciones o variables globales
-
-        Hero* hero1{new Hero(5, 3, 1, 2, 3, 1, new std::string[4]{{"fire ball"}, {"fenix tail"}, {"war growl"}, {"fly"}})};
-
-        /*Hero** heroList
+        std::cout << "Hero: " << i + 1 << std::endl;
+        for (int j{}; j < 4; j++)
         {
-            new Hero*[4]
-            {
-                new Hero(5, 3, 1, 2, 3, 1, new std::string[4]{{"fire ball"}, {"fenix tail"}, {"war growl"}, {"fly"}}),
-                new Hero(3, 3, 1, 2, 3, 1, new std::string[4]{{"water heal"}, {"erik do something"}, {"rays sword"}, {"dvdsvv"}}),
-                new Hero(6, 3, 1, 2, 3, 1, new std::string[4]{{"vsdv"}, {"svsdv"}, {"sdvsdv"}, {"sdvfv"}}),
-                new Hero(1, 3, 1, 2, 3, 1, new std::string[4]{{"fdvdr"}, {"webg"}, {"bbbb"}, {"nnnn"}})
-            }
-        };
+            std::cout << "Skill: " << *((*(heroList + i))->GetSkills() + j) << std::endl;
+        }
+        std::cout << std::endl;
+    }
 
-        for(int i{}; i < 4; i++)
-        {
-         
-                std::cout << "Health" << **(heroList + i) << std::endl;
-            
-        }*/
+    // for(int i{}; i < 4; i++)
+    // {
 
-        /*std::cout << "Hero Skills:" << std::endl;
+    //         std::cout << "Health" << **(heroList + i) << std::endl;
+
+    // }
+
+    /*std::cout << "Hero Skills:" << std::endl;
 
         for(int i{}; i < 4; i++)
         {
@@ -45,9 +52,7 @@ int main()
     std::cout << "Health: " << hero1->GetHealth() << std::endl;
     std::cout << "Lore: : " << hero1->GetLore() << std::endl;*/
 
-
-
-        /*Character* character1 = {new Character(5, 3, 1, 2, 3)};
+    /*Character* character1 = {new Character(5, 3, 1, 2, 3)};
         
         std::cout << "Character Info: " << std::endl;
         std::cout << "Health: " << character1->GetHealth() << std::endl;
@@ -62,11 +67,9 @@ int main()
         std::cout << "Lore: : " << character1->GetLore() << std::endl;
         delete &character1;*/
 
+    //character1->GetDamage(1);
+    //std::cout << "Health: " << character1->GetHealth() << std::endl;
 
-        //character1->GetDamage(1);
-        //std::cout << "Health: " << character1->GetHealth() << std::endl;
-
-
-        std::cin.get();
-        return 0;
-    }
+    std::cin.get();
+    return 0;
+}
